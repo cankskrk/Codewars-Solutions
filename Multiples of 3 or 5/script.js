4c04ffdb7d8276fcba219254d2eed6e72077b61b
cankskrk <ardacankisakurek@gmail.com>
2022-10-06 02:47:23 +0200

    add new solution

---
+++ Multiples of 3 or 5/script.js
@@ -0,0 +1,16 @@
+const solution = (number) => {
+    let sum = 0
+
+    for (let i = 0; i < number; i++) {
+        if (i % 3 == 0) {
+            sum += i
+        }
+        else if (i % 5 == 0) {
+            sum += i
+        }
+    }
+
+    return console.log(sum)
+}
+
+solution(20)
